24afad71ef936f01d39364f96337fe2403bd40ce
Dan Korostelev <nadako@gmail.com>
2017-11-01 22:37:59 +0300

    report coverage to codecov

--- test/TestMain.hx
+++ test/TestMain.hx
@@ -20,15 +20,54 @@ class TestMain {
 		Report.create(runner).setHandler(function(report) {
 			Sys.println(report.getResults());
 			#if mcover
-			var logger = mcover.coverage.MCoverage.getLogger();
-			var client = new mcover.coverage.client.PrintClient();
-			client.includeExecutionFrequency = false;
-			client.includePackageBreakdown = false;
-			logger.addClient(client);
-			logger.report();
-			Sys.println(client.output);
+			reportCoverage();
 			#end
 		});
 		runner.run();
 	}
+
+	#if mcover
+	static function reportCoverage() {
+		var logger = mcover.coverage.MCoverage.getLogger();
+		var client = new mcover.coverage.client.PrintClient();
+		client.includeExecutionFrequency = false;
+		client.includePackageBreakdown = false;
+		logger.addClient(client);
+		logger.report();
+		Sys.println(client.output);
+
+		// taken and adapted from https://github.com/HaxeCheckstyle/haxe-checkstyle
+		var report:CoverageJson = { coverage: {} };
+		var classes = logger.coverage.getClasses();
+		for (cls in classes) {
+			var coverageData:Array<LineCoverageResult> = [null];
+			var results = cls.getResults();
+			for (i in 1...results.l) coverageData[i] = 1;
+
+			var missingStatements = cls.getMissingStatements();
+			for (stmt in missingStatements) {
+				for (line in stmt.lines) coverageData[line] = 0;
+			}
+			var missingBranches = cls.getMissingBranches();
+			for (branch in missingBranches) {
+				if (branch.lines.length <= 0) continue;
+				var count = 0;
+				if (branch.trueCount > 0) count++;
+				if (branch.falseCount > 0) count++;
+				var line = branch.lines[branch.lines.length - 1];
+				coverageData[line] = count + "/2";
+			}
+
+			var c = StringTools.replace(cls.name, ".", "/") + ".hx";
+			report.coverage[c] = coverageData;
+		}
+		sys.io.File.saveContent("coverage.json", haxe.Json.stringify(report));
+	}
+	#end
 }
+
+typedef CoverageJson = {
+	var coverage:haxe.DynamicAccess<Array<LineCoverageResult>>;
+}
+
+typedef LineCoverageResult = Dynamic;
